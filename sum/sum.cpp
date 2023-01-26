776b5d5a392bf15c5eed93eb193b4284365972fa
alv1503 <alvaroarbona2003@gmail.com>
2023-01-26 12:51:58 +0100

    Versión 1 sum.cpp

---
+++ sum/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int suma = 0;
+    
+    cout << "Dime un número: "; cin >> num;
+    
+    cout << "\nLa suma entre 1 y " << num << " es: " << suma << endl;
+    
+}
