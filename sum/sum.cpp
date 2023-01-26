a14cfc00cd00ba5dcda61aa8851ff12a8caebec7
alv1503 <alvaroarbona2003@gmail.com>
2023-01-26 13:16:39 +0100

    Update sum.cpp

--- sum/sum.cpp
+++ sum/sum.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 float Suma(float num, float suma);
+bool ComprobarNegativo(float num);
 
 int main(){
     float num = 0;
-    float suma = 0;
     cout << "Dime un número: "; cin >> num;
+    if (ComprobarNegativo(num)){
+        cout << "El número introducido es negativo" << endl;
+        
+        return 6;
+    }
+    
+    float suma = 0;
+    
     
     suma = Suma(num, suma);
     
@@ -17,4 +25,11 @@ int main(){
 float Suma(float num, float suma){
     suma += (num * (1 + num))/2;
     return suma;
+}
+
+bool ComprobarNegativo(float num){
+    if (num < 1){
+        return true;
+    }
+    return false;
 }
