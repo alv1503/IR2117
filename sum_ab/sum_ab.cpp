7638cf13ccc61bf8b1d6e1f882c4c888a8838b3d
alv1503 <alvaroarbona2003@gmail.com>
2023-01-31 11:46:41 +0100

    v1 sum_ab.cpp

---
+++ sum_ab/sum_ab.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float a, b, sum = 0;
+    cout << "Dime un número >=1: "; cin >> a;
+    cout << "Dime un número >=1: "; cin >> b;
+    
+    cout << "La suma de los números entre " << a << " y " << b << " es: " << sum;
+}
