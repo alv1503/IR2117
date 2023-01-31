f9af37b3240e65c47a880a604e8107a6eab1ea67
alv1503 <alvaroarbona2003@gmail.com>
2023-01-31 11:53:25 +0100

    Update sum_ab.cpp

--- sum_ab/sum_ab.cpp
+++ sum_ab/sum_ab.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main(){
-    float a, b, sum = 0;
-    cout << "Dime un número >=1: "; cin >> a;
-    cout << "Dime un número >=1: "; cin >> b;
+    float a = 0, b = 0, sum = 0;
     
-    cout << "La suma de los números entre " << a << " y " << b << " es: " << sum;
+    while (a <= 0) {cout << "Dime un número >=1 para a: "; cin >> a;}
+        
+    while (b <= 0) {cout << "\nDime un número >=1 para b: "; cin >> b;}
+    
+    for (int i = a; i <= b; i++){
+        sum += i;
+    }
+    
+    cout << "\nLa suma de los números entre " << a << " y " << b << " es: " << sum << endl;
 }
