0a69eea51c671917a7ff556f93ae6ca055866bb3
alv1503 <alvaroarbona2003@gmail.com>
2023-02-07 12:48:01 +0100

    v1 quadratic.cpp

---
+++ quadratic/quadratic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, x1 = 0, x2 = 0;
+    
+    cout << "Dime el valor del valor a (ax²): "; cin >> a;
+    
+    cout << "Dime el valor del valor b (bx): "; cin >> b;
+    
+    cout << "Dime el valor del valor c: "; cin >> c;
+    
+    
+    cout << "Las soluciónes de la equación " << a << "x² + " << b << "x + " << c << " son:\n\tx1 = " << x1 << "\n\tx2 = " << x2 << endl;
+    
+    
+    return 0;
+}
