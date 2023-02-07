03cacc2309505ff760f2f1c1f249f867f93d1972
alv1503 <alvaroarbona2003@gmail.com>
2023-02-07 13:00:45 +0100

    Update quadratic.cpp

--- quadratic/quadratic.cpp
+++ quadratic/quadratic.cpp
@@ -14,10 +14,16 @@ int main(){
     
     double discriminant = (b*b - 4*a*c);
     
-    x1 = (-b + sqrt(discriminant))/(2*a);
-    x2 = (-b - sqrt(discriminant))/(2*a);
+    if (discriminant < 0){
+        cout << "No hay solución" << endl;
+    }
     
-    cout << "Las soluciónes de la equación " << a << "x² + " << b << "x + " << c << " son:\n\tx1 = " << x1 << "\n\tx2 = " << x2 << endl;
+    else{
+        x1 = (-b + sqrt(discriminant))/(2*a);
+        x2 = (-b - sqrt(discriminant))/(2*a);
+        
+        cout << "Las soluciónes de la equación " << a << "x² + " << b << "x + " << c << " son:\n\tx1 = " << x1 << "\n\tx2 = " << x2 << endl;
+    }
     
     
     return 0;
