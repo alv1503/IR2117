304efc76c2c75109075906c6012c2476d7b770cb
alv1503 <alvaroarbona2003@gmail.com>
2023-02-07 12:58:11 +0100

    v2 quadratic.cpp

--- quadratic/quadratic.cpp
+++ quadratic/quadratic.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main(){
-    int a, b, c, x1 = 0, x2 = 0;
+    int a, b, c;
+    float x1, x2;
     
     cout << "Dime el valor del valor a (ax²): "; cin >> a;
     
@@ -11,6 +12,10 @@ int main(){
     
     cout << "Dime el valor del valor c: "; cin >> c;
     
+    double discriminant = (b*b - 4*a*c);
+    
+    x1 = (-b + sqrt(discriminant))/(2*a);
+    x2 = (-b - sqrt(discriminant))/(2*a);
     
     cout << "Las soluciónes de la equación " << a << "x² + " << b << "x + " << c << " son:\n\tx1 = " << x1 << "\n\tx2 = " << x2 << endl;
     
