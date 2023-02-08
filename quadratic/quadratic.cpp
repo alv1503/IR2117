13a26d1baa50ffd3b509ba25421834904a7958aa
alv1503 <alvaroarbona2003@gmail.com>
2023-02-08 10:42:47 +0100

    v5 quadratic.cpp

--- quadratic/quadratic.cpp
+++ quadratic/quadratic.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-    int a, b, c;
-    float x1, x2;
+    double a, b, c;
+    double x1, x2;
     
     cout << "Dime el valor del valor a (ax²): "; cin >> a;
     
@@ -15,11 +15,15 @@ int main(){
     double discriminant = (b*b - 4*a*c);
     
     if (discriminant < 0){
-        cout << "No hay solución" << endl;
+        double x_real = (-b)/(2*a);
+        double x_imag = sqrt(abs(discriminant))/(2*a);
+        
+        cout << "Las soluciónes de la equación " << a << "x² + " << b << "x + " << c << " son:\n\tx1 = " << x_real << " + " << x_imag << "i \n\tx2 = " << x_real << " - " << x_imag << "i" << endl;
     }
     
     else if(discriminant == 0){
         x1 = (-b)/(2*a);
+        
         cout << "La solución de la equación " << a << "x² + " << b << "x + " << c << " es:\n\tx1 = " << x1 << endl;
     }
     
@@ -29,7 +33,6 @@ int main(){
         
         cout << "Las soluciónes de la equación " << a << "x² + " << b << "x + " << c << " son:\n\tx1 = " << x1 << "\n\tx2 = " << x2 << endl;
     }
-    
     
     return 0;
 }
