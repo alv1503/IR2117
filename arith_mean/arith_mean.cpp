de80f3f7430d0482e714fd50289242a6f2efe023
alv1503 <alvaroarbona2003@gmail.com>
2023-02-14 11:31:31 +0100

    arith_mean v3

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv){
     double m = 0, s = 0;
     int n = argc - 1;
     
-    for (int i = 1; i <= argc; i++){
-        s += argv[i];
+    for (int i = 1; i <= n; i++){
+        s += atoi(argv[i]);
     }
     
     m = s/n
