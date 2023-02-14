e3a151aa53595b1f28e150ec318c82f679b7a4e0
alv1503 <alvaroarbona2003@gmail.com>
2023-02-14 11:30:32 +0100

    arith_mean v2

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main(int argc, char** argv){
-    double m = 0;
+    double m = 0, s = 0;
+    int n = argc - 1;
+    
+    for (int i = 1; i <= argc; i++){
+        s += argv[i];
+    }
+    
+    m = s/n
     
     cout << "Arithmetic mean: " << m << endl;
     
