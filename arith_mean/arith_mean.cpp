bb194ab1c80750c55ba50601f0ed6c06bea7ae24
alv1503 <alvaroarbona2003@gmail.com>
2023-02-14 12:22:46 +0100

    arith_mean v7

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int number, n;
+int main(int argc, char** argv){
+    int number, n = 0;
     double sum, mean;
+    
+    vector<int> numbers;
     
     cin >> number;
     
     while (not cin.eof()){
-        cin >> number;
-        sum += number;
+        numbers.push_back(number);
         n++;
+        cin >> number;
     }
+    
+    for (int i = 0; i < n; i++){
+        sum += numbers[i];
+    }   
     
     mean = sum/n;
     
