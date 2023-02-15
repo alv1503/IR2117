f6d99b2e4cddf80538d7bf8c42b0ec72ac184ab9
alv1503 <alvaroarbona2003@gmail.com>
2023-02-15 18:06:52 +0100

    mode v1

--- arith_mean/mode.cpp
+++ arith_mean/mode.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char** argv){
     int number, n = 0;
-    double sum, mean;
+    double max = 0, mode;
     
     vector<int> numbers;
     
@@ -13,13 +13,17 @@ int main(int argc, char** argv){
     
     while (not cin.eof()){
         numbers.push_back(number);
-        n++;
         cin >> number;
     }
     
+    for (int i = 0; i < numbers.size(); i++){
+        if (numbers[0] == numbers[i]){
+            n++;
+        }
+    }
     
-    
-    cout << "Mode: " << mean << endl;
+    cout << numbers[0] << " aparece " << n << " veces" << endl;  
+    //cout << "Mode: " << mean << endl;
     
     return 0;
 }
