51cea408f69a74bc1e23950ce74e041130cf1805
alv1503 <alvaroarbona2003@gmail.com>
2023-02-23 11:48:53 +0100

    cambio en square
    cambio el nombre del nodo

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -7,7 +7,7 @@ using namespace std::chrono_literals;
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("publisher");
+  auto node = rclcpp::Node::make_shared("square");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(10ms);
