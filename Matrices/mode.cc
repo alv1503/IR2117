ab8c06d89f5c4158004b01867c88aa531b39a37d
alv1503 <alvaroarbona2003@gmail.com>
2023-02-26 18:38:02 +0100

    mode.cc v3

--- Matrices/mode.cc
+++ Matrices/mode.cc
@@ -6,18 +6,19 @@ int main(){
     Eigen::VectorXd v(8);
     v << 2, 4, 5, 2, 2, 6, 7, 2;
     
-    n = 0;
     for (int i = 0; i < v.size(); i++){
-        if (v[i] == v[0]){
-            n++;
+        n = 0;
+        for (int j = 0; j < v.size(); j++){
+            if (v[i] == v[j]){
+                n++;
+            }
+        }
+        
+        if (n > max){
+            max = n;
+            mode = v[0];
         }
     }
-    
-    if (n > max){
-        max = n;
-        mode = v[0];
-    }
-    
     std::cout << "Mode: " << mode << std::endl;
     
     return 0;
