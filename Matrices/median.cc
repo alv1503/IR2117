76c5ab567f8373dfb6fc88d6721d3a2fa0181100
alv1503 <alvaroarbona2003@gmail.com>
2023-02-26 18:20:23 +0100

    medina.cc v2

--- Matrices/median.cc
+++ Matrices/median.cc
@@ -11,6 +11,8 @@ int main(){
     //Eigen::VectorXd v(7);
     //v << 5, 2, 4, 3, 7, 8, 1;
     
+    std::sort(v.data(), v.data() + v.size());
+    
     std::cout << "La mediana es: " << median << std::endl;
     
     return 0;
