56b8769062ec8933ec17554f2ca5c2db69a957e9
alv1503 <alvaroarbona2003@gmail.com>
2023-02-26 17:36:02 +0100

    example 2.2

--- Matrices/example.cc
+++ Matrices/example.cc
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
+using Eigen::Matrix3d;
+using Eigen::Vector3d;
 
 int main(){
-    MatrixXd m = MatrixXd::Random(3,3);
-    m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+    Matrix3d m = Matrix3d::Random(3,3);
+    m = (m + Matrix3d::Constant(3,3,1.2)) * 50;
     
     std::cout << "m = " << std::endl << m << std::endl;
     
-    VectorXd v(3);
+    Vector3d v(3);
     v << 1, 2, 3;
     
     std::cout << "m * v = " << std::endl << m * v << std::endl;
