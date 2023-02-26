e82d9fd483d04bc497301e29367b6bffcb0bbf1b
alv1503 <alvaroarbona2003@gmail.com>
2023-02-26 18:20:01 +0100

    median.cc v1

---
+++ Matrices/median.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <algorithm>
+
+int main(){
+    double median;
+    
+    Eigen::VectorXd v(4);
+    v << 4, 7, 1, 12;
+    
+    //Eigen::VectorXd v(7);
+    //v << 5, 2, 4, 3, 7, 8, 1;
+    
+    std::cout << "La mediana es: " << median << std::endl;
+    
+    return 0;
+}
