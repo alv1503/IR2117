a6b86bc3c3b305dc1980b142b590190fb4e05822
alv1503 <alvaroarbona2003@gmail.com>
2023-02-26 20:36:40 +0100

    mode.cc v3

--- Matrices/mode.cc
+++ Matrices/mode.cc
@@ -2,22 +2,24 @@
 #include <Eigen/Dense>
 
 int main(){
-    int mode, n, max = 0;
-    Eigen::VectorXd v(8);
-    v << 2, 4, 5, 2, 2, 6, 7, 2;
+    int mode, n, max = 0, number;
+    Eigen::VectorXd v(7);
     
-    n = 0;
+    v << 1, 2, 2, 6, 4, 2, 9;
+    
     for (int i = 0; i < v.size(); i++){
-        if (v[i] == v[0]){
-            n++;
+        n = 0;
+        for (int j = 0; j < v.size(); j++){
+            if (v[i] == v[j]){
+                n++;
+            }
+        }
+        
+        if (n > max){
+            max = n;
+            mode = v[i];
         }
     }
-    
-    if (n > max){
-        max = n;
-        mode = v[0];
-    }
-    
     std::cout << "Mode: " << mode << std::endl;
     
     return 0;
