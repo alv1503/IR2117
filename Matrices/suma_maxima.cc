686668a7d651df159244a074b9bf7b082ad0f6f3
alv1503 <alvaroarbona2003@gmail.com>
2023-02-28 11:59:55 +0100

    suma_maxima.cc v1

--- Matrices/suma_maxima.cc
+++ Matrices/suma_maxima.cc
@@ -4,15 +4,20 @@
 using Eigen::MatrixXd;
 
 int main(){
-    int rows, cols, sum = 0;
+    int rows, cols, sum_tot = 0, sum_row = 0, sum_cols = 0, sum_diagonal1 = 0, sum_diagonal2 = 0;
     
     std::cout << "Columnas: "; std::cin >> cols;
     std::cout << "Filas: "; std::cin >> rows;
     
     MatrixXd m(rows, cols);std::cout << "Enter elements (row-wise): " << std::endl;
 
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            std::cin >> m(i,j);
+        }
+    }
+    
     std::cout << "The matrix is: " << std::endl;
     std::cout << m << std::endl;
-    
-    std::cout << "La suma de los elementos de la matriz es: " << sum << std::endl;
+    std::cout << "La suma de los elementos de la matriz es: " << sum_tot << std::endl;
 }
