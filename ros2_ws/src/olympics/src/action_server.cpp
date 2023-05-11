f2206feda11a71de2aa5ebd1aaefb64dd98f7ae4
alv1503 <alvaroarbona2003@gmail.com>
2023-05-11 16:52:54 +0200

    ERROR EN COLCON BUILD V2

---
+++ ros2_ws/src/olympics/src/action_server.cpp
@@ -0,0 +1,111 @@
+#include <inttypes.h>
+#include <memory>
+#include "olympic_interfaces/action/rings.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using Rings = 
+  action_tutorials_interfaces::action::Rings;
+
+using GoalHandleRings = 
+  rclcpp_action::ServerGoalHandle<Rings>;
+
+rclcpp_action::GoalResponse handle_goal(
+  const rclcpp_action::GoalUUID & uuid, 
+  std::shared_ptr<const Rings::Goal> goal)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Got goal request with order %d", goal->order);
+  (void)uuid;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse handle_cancel(
+  const std::shared_ptr<GoalHandleRings> goal_handle)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Got request to cancel goal");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void execute(const std::shared_ptr<GoalHandleRings>);
+
+void handle_accepted(
+  const std::shared_ptr<GoalHandleRings> goal_handle)
+{
+  std::thread{execute, goal_handle}.detach();
+}
+
+void execute(
+  const std::shared_ptr<GoalHandleRings> goal_handle)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Executing goal");
+  rclcpp::Rate loop_rate(1);
+  const auto goal = goal_handle->get_goal();
+  auto feedback = std::make_shared<Rings::Feedback>();
+  auto & sequence = feedback->partial_sequence;
+  sequence.push_back(0);
+  sequence.push_back(1);
+  auto result = std::make_shared<Rings::Result>();
+  
+  for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
+    if (goal_handle->is_canceling()) {
+      result->sequence = sequence;
+      goal_handle->canceled(result);
+      RCLCPP_INFO(rclcpp::get_logger("server"), 
+        "Goal Canceled");
+      return;
+    }
+    
+    sequence.push_back(sequence[i] + sequence[i - 1]);
+    goal_handle->publish_feedback(feedback);
+    RCLCPP_INFO(rclcpp::get_logger("server"), 
+      "Publish Feedback");
+    loop_rate.sleep();
+  }
+  
+  if (rclcpp::ok()) {
+    result->sequence = sequence;
+    goal_handle->succeed(result);
+    RCLCPP_INFO(rclcpp::get_logger("server"), 
+      "Goal Succeeded");
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("olympics_action_server");
+  auto action_server = 
+    rclcpp_action::create_server<Rings>(node,
+      "olympics",
+      handle_goal,
+      handle_cancel,
+      handle_accepted);
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
