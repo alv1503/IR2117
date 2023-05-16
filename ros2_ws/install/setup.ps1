74369b5356e0f55f669d8794b88a85d6a5fd5c90
alv1503 <alvaroarbona2003@gmail.com>
2023-05-16 12:14:28 +0200

    error

--- ros2_ws/install/setup.ps1
+++ ros2_ws/install/setup.ps1
@@ -21,6 +21,10 @@ function _colcon_prefix_chain_powershell_source_script {
   }
 }
 
+# source chained prefixes
+_colcon_prefix_chain_powershell_source_script "/opt/ros/foxy\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/alvaro/Documents/GitHub/IR2117/ros2_ws/src/install\local_setup.ps1"
+
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
 _colcon_prefix_chain_powershell_source_script "$env:COLCON_CURRENT_PREFIX\local_setup.ps1"
